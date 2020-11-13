b59b51487ef3b320aa820120e4b221a90f962756
Rama Sonko <businessitsquiet@gmail.com>
2020-11-13 12:31:38 -0500

    first commit

---
+++ bot.py
@@ -0,0 +1,50 @@
+# author: @ryreadme
+# Trading Bot
+
+# Skeleton Version: bot is not finished
+
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+from datetime import datetime
+import time
+
+symbol = None
+
+
+def get_account_balance():
+    pass
+
+
+def get_stock_data():
+    pass
+
+
+def market_price():
+    pass
+
+
+def drawMarket():
+    pass
+
+
+def buy_threshold():
+    pass
+
+
+def sell_threshold():
+    pass
+
+
+def buy():
+    pass
+
+
+def sell():
+    pass
+
+# TO DO:
+# Find a cloud provider
+# Find an API to use for the exchange
+# Decide threshold where is it okay to buy or sell
+# Check stocks every 30s or min whichever is possible
